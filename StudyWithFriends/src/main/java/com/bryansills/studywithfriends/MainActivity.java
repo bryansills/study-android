3f1a923280b185a00dfe0e38f9722a5017ccda4c
bryansills <bryan.sills@gatech.edu>
2013-11-03 01:43:32 -0400

    changed target sdk to 19 and added actionbarcompat

--- StudyWithFriends/src/main/java/com/bryansills/studywithfriends/MainActivity.java
+++ StudyWithFriends/src/main/java/com/bryansills/studywithfriends/MainActivity.java
@@ -1,10 +1,10 @@
 package com.bryansills.studywithfriends;
 
 import android.os.Bundle;
-import android.app.Activity;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 
-public class MainActivity extends Activity {
+public class MainActivity extends ActionBarActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
