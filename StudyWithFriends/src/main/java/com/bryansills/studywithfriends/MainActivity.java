da1e1cfac268b6b1c40259ae5b76cdf83dc93d15
bryansills <bryan.sills@gatech.edu>
2013-12-22 19:16:46 -0500

    added drawer and menu items for settings and help

--- StudyWithFriends/src/main/java/com/bryansills/studywithfriends/MainActivity.java
+++ StudyWithFriends/src/main/java/com/bryansills/studywithfriends/MainActivity.java
@@ -1,13 +1,97 @@
 package com.bryansills.studywithfriends;
 
 import android.app.Activity;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
+    private DrawerLayout drawer;
+    private ActionBarDrawerToggle drawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        drawer = (DrawerLayout) findViewById(R.id.drawer);
+
+        // set a custom shadow that overlays the main content when the drawer opens
+        drawer.setDrawerShadow(R.drawable.drawer_shadow, GravityCompat.START);
+
+        // enable ActionBar app icon to behave as action to toggle nav drawer
+        getActionBar().setDisplayHomeAsUpEnabled(true);
+        getActionBar().setHomeButtonEnabled(true);
+
+        // ActionBarDrawerToggle ties together the the proper interactions
+        // between the sliding drawer and the action bar app icon
+        drawerToggle = new ActionBarDrawerToggle(
+                this,                  /* host Activity */
+                drawer,         /* DrawerLayout object */
+                R.drawable.ic_drawer,  /* nav drawer image to replace 'Up' caret */
+                R.string.drawer_open,  /* "open drawer" description for accessibility */
+                R.string.drawer_close  /* "close drawer" description for accessibility */
+        ) {
+            public void onDrawerClosed(View view) {
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            public void onDrawerOpened(View drawerView) {
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+        drawer.setDrawerListener(drawerToggle);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // The action bar home/up action should open or close the drawer.
+        // ActionBarDrawerToggle will take care of this.
+        if (drawerToggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+        // Handle action buttons
+        switch(item.getItemId()) {
+            case R.id.settings:
+                Toast.makeText(this, R.string.settings, Toast.LENGTH_LONG).show();
+                return true;
+            case R.id.help:
+                Toast.makeText(this, R.string.help, Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    /**
+     * When using the ActionBarDrawerToggle, you must call it during
+     * onPostCreate() and onConfigurationChanged()...
+     */
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        drawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        // Pass any configuration change to the drawer toggls
+        drawerToggle.onConfigurationChanged(newConfig);
     }
 }
--- StudyWithFriends/src/test/java/com/bryansills/studywithfriends/MainActivityTest.java
+++ StudyWithFriends/src/test/java/com/bryansills/studywithfriends/MainActivityTest.java
@@ -1,19 +1,63 @@
 package com.bryansills.studywithfriends;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.drawable.Drawable;
+import android.support.v4.app.ActionBarDrawerToggle;
+import android.support.v4.widget.DrawerLayout;
+import android.view.ActionProvider;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.SubMenu;
+import android.view.View;
+import com.bryansills.studywithfriends.MainActivity;
+import dalvik.annotation.TestTargetClass;
 import org.junit.Test;
+import org.junit.Before;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
+import org.robolectric.shadows.ShadowToast;
+import org.robolectric.tester.android.view.TestMenuItem;
 import com.bryansills.studywithfriends.RobolectricGradleTestRunner;
+
+import java.lang.*;
+import java.lang.CharSequence;
+import java.lang.Exception;
+import java.lang.Override;
+import java.lang.System;
 
 import static org.junit.Assert.assertTrue;
 
 @RunWith(RobolectricGradleTestRunner.class)
 public class MainActivityTest {
+    private MainActivity mainActivity;
+    private DrawerLayout drawer;
+    private MenuItem settingsMenuItem;
+    private MenuItem helpMenuItem;
+
+    @Before
+    public void setUp() throws Exception {
+        mainActivity = Robolectric.buildActivity(MainActivity.class).create().visible().get();
+        drawer = (DrawerLayout) mainActivity.findViewById(R.id.drawer);
+        settingsMenuItem = new TestMenuItem(R.id.settings);
+        helpMenuItem = new TestMenuItem(R.id.help);
+    }
 
     @Test
     public void testActivity() throws Exception {
-        Activity activity = Robolectric.buildActivity(MainActivity.class).create().get();
-        assertFalse(activity != null);
+        assertTrue(mainActivity != null);
+    }
+
+    @Test
+    public void settingsMenuItemShouldStartSettingsActivity() throws Exception {
+        mainActivity.onOptionsItemSelected(settingsMenuItem);
+        assertTrue(ShadowToast.getTextOfLatestToast().equals("Settings"));
+    }
+
+    @Test
+    public void helpMenuItemShouldStartSettingsActivity() throws Exception {
+        mainActivity.onOptionsItemSelected(helpMenuItem);
+        assertTrue(ShadowToast.getTextOfLatestToast().equals("Help"));
     }
 }
